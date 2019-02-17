b31ee2ba320d9433a35464c18d62170ca39c843c
amith001 <vinayakamith10@gmail.com>
2019-02-17 15:13:30 +0530

    Updates

---
+++ precache-manifest.3c4f321d41f5e08a27d6e0fc0fcd4499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd9a3109b627deb1fec9",
+    "url": "/list/static/css/main.652d8337.chunk.css"
+  },
+  {
+    "revision": "bd9a3109b627deb1fec9",
+    "url": "/list/static/js/main.bd9a3109.chunk.js"
+  },
+  {
+    "revision": "5acea4e19c31425e3c49",
+    "url": "/list/static/js/runtime~main.5acea4e1.js"
+  },
+  {
+    "revision": "730ae9dc66c63584a64d",
+    "url": "/list/static/js/2.730ae9dc.chunk.js"
+  },
+  {
+    "revision": "0123ba91a45e85801f1f9dd10b0b0e85",
+    "url": "/list/index.html"
+  }
+];
